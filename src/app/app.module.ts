77687e3a4380ca50ee6858f894c840df995cf61d
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2022-03-16 17:56:40 +0600

    Basic File structure created

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { HeaderComponent } from './core/components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
