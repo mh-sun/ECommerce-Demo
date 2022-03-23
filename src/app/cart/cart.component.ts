f25a217039232abdbc9b884e71f928a61eaf89fd
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2022-03-23 14:32:39 +0600

    Cart complete

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,17 +7,24 @@ import { CartApiService } from '../core/services/cart-api.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  public products : any = [{"id":1,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}];
+  public products : any = [];
   public grandTotal !: number;
   constructor(private cartService : CartApiService) { }
 
   ngOnInit(): void {
-    
+    this.cartService.getProducts().subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.products = res
+        this.grandTotal = this.cartService.getTotalPrice()
+        console.log(this.grandTotal)
+      }
+    })
   }
   removeItem(item: any){
-    // this.cartService.removeCartItem(item);
+    this.cartService.removeCartItem(item);
   }
   emptycart(){
-    // this.cartService.removeAllCart();
+    this.cartService.removeAllCart();
   }
 }
