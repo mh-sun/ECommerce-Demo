38e0fa65cb1cb556aaa25e77617a4460c9a93271
atkia <bsse1015@iit.du.ac.bd>
2022-04-02 17:20:46 +0600

    add product dialog added

---
+++ src/app/admin/add-product/add-product.component.ts
@@ -0,0 +1,80 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Product } from 'src/app/core/models/product.model';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+export class AddProductComponent implements OnInit {
+  obj = new Object;
+  addVariant = false;
+  product:Product|any;
+  imageLink:string|any;
+  productForm = this.fb.group({
+    title:[''],
+    price:[''],
+    description:[''],
+    category:[''],
+    image:[''],
+    variation:['']
+ });
+
+  constructor(
+    public dialogRef: MatDialogRef<SidenavComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,private fb:FormBuilder) {
+      console.log(data)
+     }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {
+  }
+  imagePreview(e:any) {
+    let event = (e.target as HTMLInputElement).files;
+    let file:any;
+    if(event!=null){
+       file = event[0];
+      this.productForm.patchValue({
+        image: file
+      });
+    }
+
+    let img = this.productForm.get('image');
+    if(img!=null){
+      img.updateValueAndValidity()
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageLink = reader.result as string;
+    }
+    reader.readAsDataURL(file)
+  }
+  addMore(input1:string,input2:string){
+    console.log(input1,input2);
+    let arr = new Array(input2);
+    
+    Object.assign(this.obj,{[input1]:input2})
+    console.log(this.obj)
+    this.productForm.patchValue({
+      variation: this.obj
+    });
+    let variant = this.productForm.get('variation');
+    console.log(variant?.value)
+    if(variant!=null){
+      variant.updateValueAndValidity()
+    }
+    this.addVariant=false;
+  }
+
+  onSubmit(){
+    console.log(this.productForm.value);
+    this.dialogRef.close();
+  }
+
+}
--- src/app/admin/sidenav/sidenav.component.ts
+++ src/app/admin/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddProductComponent } from '../add-product/add-product.component';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-
-  constructor() { }
+  
+  animal!: string;
+  name!: string;
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+  openDialog(): void {
+    let dialogRef = this.dialog.open(AddProductComponent, {
+      // width: '250px',
+      // data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
   }
 
 }
