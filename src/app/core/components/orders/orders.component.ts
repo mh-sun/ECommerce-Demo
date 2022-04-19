cbe06119ea771b8c506123051034e76782642084
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2022-04-19 14:34:36 +0600

    order status dynamic

--- src/app/core/components/orders/orders.component.ts
+++ src/app/core/components/orders/orders.component.ts
@@ -16,37 +16,46 @@ export class OrdersComponent implements OnInit {
   name = 'Progress Bar';
   user: User|any;
   public counts = ["Payment Pending","Processing","Shipped","Delivered"];
-  public orderStatus = "Delivered";
   totalBill=0;
   order:Order|any;
+  orderStatusIndex :number = -1
   subscriptionName!: Subscription;
-  constructor(private service:ProductsService,private userService:LogService,private route:ActivatedRoute,private log:LogService,private router:Router,private orderService:OrderService) {   
-  
-  }
+  constructor(
+    private service:ProductsService,
+    private userService:LogService,
+    private route:ActivatedRoute,
+    private log:LogService,
+    private router:Router,
+    private orderService:OrderService
+  ) {}
   ngOnInit(): void {
     let orders:Order[]|undefined;
     const routeParams = this.route.snapshot.paramMap;
     let orderId = String(routeParams.get('id'));
+    console.log(this.orderStatusIndex)
     this.orderService.currentOrder.subscribe({
       next:order=>{
+        console.log(this.orderStatusIndex)
         this.order = order;
         if(order===null) return
+        this.orderStatusIndex = this.counts.indexOf(order.status)
         this.totalBill = this.order.payment.shipping+this.order.payment.subtotal;
       }
     })
     this.log.loggedUser.subscribe({
       next:u=>{
+        console.log(this.orderStatusIndex)
         this.user = u;
-        if(this.order==null){
+        if(this.order===null){
           orders = u?.orders;
-              if(orders!=null){
-                for(let order of orders){
-                  if(order.id==orderId){
-                    this.order = order;
-                    this.totalBill = this.order.payment.shipping+this.order.payment.subtotal;
-                  }
-                }
+          if(orders!=null){
+            for(let order of orders){
+              if(order.id==orderId){
+                this.order = order;
+                this.totalBill = this.order.payment.shipping+this.order.payment.subtotal;
               }
+            }
+          }
         }
       }
     })
